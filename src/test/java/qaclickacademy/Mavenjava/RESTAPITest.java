330c9e5e9c4607a0b5c103391721200b72616ebb
nithyansenthilvel <nithyansenthilvel@gmail.com>
2020-05-16 13:26:39 +0530

    second commit

--- src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -14,5 +14,8 @@ public class RESTAPITest {
 	@Test
 	public void deleteTwitter() {
 		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter1");
+		System.out.println("deleteTwitter2");
+		
 	}
 }
